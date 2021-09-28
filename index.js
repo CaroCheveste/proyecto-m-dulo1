678409bcd6b3a863aeec79a9bc1e672480098b34
CaroCheveste <carocheveste@gmail.com>
2021-09-27 21:44:15 -0300

    27setv1

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 /* COMENTARIO DE MAS DE
 UNA LINEA*/
 
+/*window.addEventListener('scroll', function(){
+let animacion = document.getElementById('animado);
+let posicion1 = animacion.getBoundingClientRect().top;
+console.log(posicion1);
+let tamañoDePantalla = window.innerHeight/3;
 
+if(posicion1 < tamañoDePantalla) {
+    animacion.style.animation = 'mover 1s ease-out'
+}
 
+})
+*/
